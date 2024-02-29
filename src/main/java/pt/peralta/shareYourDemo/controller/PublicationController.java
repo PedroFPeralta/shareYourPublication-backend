15c928d19596825ced5c4cb1b68b1342241ee3d9
PedroFPeralta <200221137@estudantes.ips.pt>
2024-02-29 21:12:05 +0000

    Creation of PublicationController

---
+++ src/main/java/pt/peralta/shareYourDemo/controller/PublicationController.java
@@ -0,0 +1,45 @@
+package pt.peralta.shareYourDemo.controller;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pt.peralta.shareYourDemo.entity.Publication;
+import pt.peralta.shareYourDemo.entity.PublicationDTO;
+import pt.peralta.shareYourDemo.service.PublicationService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("publications")
+public class PublicationController {
+
+    private PublicationService service;
+
+    public PublicationController(PublicationService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Publication>> listAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.listAll());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Publication> create(@RequestBody PublicationDTO publicationDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(publicationDTO));
+    }
+
+    @PutMapping("/:id")
+    public ResponseEntity<Publication> update(@PathParam("id") Long id, @RequestBody PublicationDTO publicationDTO) throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(id,publicationDTO));
+    }
+
+    @DeleteMapping("/:id")
+    public ResponseEntity<Publication> delete(@PathParam("id") Long id) throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(service.delete(id));
+    }
+
+}
