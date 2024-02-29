8f838807a3ff253aeb5b7f16cdc80ba7e32fcfa7
PedroFPeralta <200221137@estudantes.ips.pt>
2024-02-29 20:45:37 +0000

    Initial Commit

---
+++ src/main/java/pt/peralta/shareYourDemo/ShareYourDemoApplication.java
@@ -0,0 +1,13 @@
+package pt.peralta.shareYourDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareYourDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareYourDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/peralta/shareYourDemo/ShareYourDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.peralta.shareYourDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareYourDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
